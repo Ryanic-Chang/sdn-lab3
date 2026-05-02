3f99a5be39d15898ca1d014b8ede0df16dff9eab
Ryanic-Chang <ryanchang1412@gmail.com>
2026-05-02 20:35:45 +0800

    feat:自学习交换机补全

--- controllers/task1/self_learning_switch.py
+++ controllers/task1/self_learning_switch.py
@@ -13,6 +13,8 @@ class Switch(app_manager.OSKenApp):
     def __init__(self, *args, **kwargs):
         super(Switch, self).__init__(*args, **kwargs)
         # maybe you need a global data structure to save the mapping
+        # 初始化一个字典保存每个交换机的 mac_to_port 映射
+        self.mac_to_port = {}
         
     def add_flow(self, datapath, priority, match, actions,idle_timeout=0,hard_timeout=0):
         dp = datapath
@@ -57,7 +59,39 @@ class Switch(app_manager.OSKenApp):
         # You need to code here to avoid the direct flooding
         # Have fun!
         # :)
+        # 1. 为当前交换机初始化 mac_to_port 字典
+        self.mac_to_port.setdefault(dpid, {})
 
+        # 2. 将源 MAC 地址与入端口绑定
+        self.mac_to_port[dpid][src] = in_port
+
+        # 3. 查询目的 MAC 地址是否已学习
+        if dst in self.mac_to_port[dpid]:
+            # 单播转发至指定端口
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            # 全网洪泛(入端口除外)
+            out_port = ofp.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # 4. 如果不是洪泛（即已知道目的端口），则向交换机下发流表
+        # 后续同类的报文不需要再上送控制器（Packet-In），而是直接由交换机硬件转发
+        if out_port != ofp.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            # 下发的流表优先级必须大于默认的 Table-Miss 流表（优先级为0），此处设为1
+            self.add_flow(dp, 1, match, actions)
+
+        # 5.让交换机把当前被拦截的包发出去
+        out = parser.OFPPacketOut(
+            datapath=dp, 
+            buffer_id=ofp.OFP_NO_BUFFER, 
+            in_port=in_port, 
+            actions=actions, 
+            data=msg.data
+        )
+        dp.send_msg(out)
+        
 if __name__ == '__main__':
     log.init_log()
     app_manager.AppManager.run_apps(["controllers.task1.self_learning_switch"])
